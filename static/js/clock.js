ce21d027bebbec7b3a49f1f81f2e97b9e413717e
study-MY <null.for.ez@gmail.com>
2024-03-20 18:31:29 +0900

    contact time and linking

---
+++ static/js/clock.js
@@ -0,0 +1,15 @@
+function updateClock() {
+  var now = new Date();
+  var hours = now.getHours().toString().padStart(2, "0");
+  var minutes = now.getMinutes().toString().padStart(2, "0");
+  var seconds = now.getSeconds().toString().padStart(2, "0");
+
+  var timeString = `${hours}:${minutes}:${seconds}`;
+  document.getElementById("time").textContent = timeString;
+}
+
+// 매 초마다 시계 업데이트
+setInterval(updateClock, 1000);
+
+// 페이지 로드 시에도 시계 업데이트
+updateClock();
